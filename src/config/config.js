2b78dbc468459d20dfe87b3f95defb500119dc95
Rafael Martinez <rrhhmmtt@gmail.com>
2023-06-06 10:14:53 -0500

    se agrega base para env

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,15 @@
+//imports app
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+//imports propios
+import { __dirname } from "../utils/utils.js";
+
+const enviroment = process.argv.slice(2)[0];
+const pathEnvironment = environment === "prod" ? path.join(__dirname,"../.env.production") : path.join(__dirname,"../.env.development");
+
+dotenv.config({
+    path: pathEnvironment
+});
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
